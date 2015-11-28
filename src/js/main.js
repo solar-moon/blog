418dff082d4beb7eb1d4d84d6305785a4ef31232
Łukasz Jan Niemier <lukasz@niemier.pl>
2015-11-28 19:53:46 +0100

    Better UI

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const TARGET = moment('2015-11-28 20:30:00+0100').utc();
 let hh = document.getElementById('H');
 let mm = document.getElementById('M');
 let ss = document.getElementById('s');
+let cc = document.getElementById('cs');
 
 let countdown = document.getElementById('countdown');
 
@@ -19,12 +20,13 @@ setInterval(() => {
 
   let h = diff.hours();
   let m = diff.minutes();
-  let cs = ~~(diff.milliseconds() / 10);
+  let c = ~~(diff.milliseconds() / 10);
   let s = diff.seconds();
-  if (h == 0 && m == 0 && s == 8 && cs < 61) {
+  if (h == 0 && m == 0 && s == 8 && c < 61) {
     countdown.play();
   }
   hh.innerHTML = h.pad(2);
   mm.innerHTML = m.pad(2);
-  ss.innerHTML = `${s.pad(2)}.${cs.pad(2)}`;
+  ss.innerHTML = s.pad(2);
+  cc.innerHTML = c.pad(2)}`;
 }, 30);
